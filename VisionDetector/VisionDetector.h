72e9168c6fd1790b69f91d7746894d5b42dfd534
Yusuf <yusuf@eftsoftware.com>
2017-08-21 17:36:13 +0300

    Project init

---
+++ VisionDetector/VisionDetector.h
@@ -0,0 +1,19 @@
+//
+//  VisionDetector.h
+//  VisionDetector
+//
+//  Created by Yusuf Miletli on 8/21/17.
+//  Copyright © 2017 Miletli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisionDetector.
+FOUNDATION_EXPORT double VisionDetectorVersionNumber;
+
+//! Project version string for VisionDetector.
+FOUNDATION_EXPORT const unsigned char VisionDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionDetector/PublicHeader.h>
+
+
---
+++ VisionDetector/VisionDetector.swift
@@ -0,0 +1,9 @@
+//
+//  VisionDetector.swift
+//  VisionDetector
+//
+//  Created by Yusuf Miletli on 8/21/17.
+//  Copyright © 2017 Miletli. All rights reserved.
+//
+
+import Foundation
